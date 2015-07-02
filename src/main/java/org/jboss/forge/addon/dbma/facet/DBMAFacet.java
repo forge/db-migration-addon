3a4a70113d41e655c898a316083fdba2fea9898d
addonis1990 <wicem.zrelly@gmail.com>
2015-07-02 14:20:24 +0100

    added an interface for the DBMAFacet

--- src/main/java/org/jboss/forge/addon/dbma/facet/DBMAFacet.java
+++ src/main/java/org/jboss/forge/addon/dbma/facet/DBMAFacet.java
@@ -6,88 +6,29 @@
  */
 package org.jboss.forge.addon.dbma.facet;
 
-import org.jboss.forge.addon.dbma.properties.DBMAPropertiesBuilder;
-import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
-import org.jboss.forge.addon.facets.AbstractFacet;
-import org.jboss.forge.addon.facets.constraints.FacetConstraint;
-import org.jboss.forge.addon.facets.constraints.FacetConstraints;
-import org.jboss.forge.addon.projects.Project;
+import java.util.Properties;
+
+import org.jboss.forge.addon.database.tools.connections.ConnectionProfile;
 import org.jboss.forge.addon.projects.ProjectFacet;
-import org.jboss.forge.addon.projects.facets.DependencyFacet;
-import org.jboss.forge.addon.projects.facets.MetadataFacet;
-import org.jboss.forge.addon.projects.facets.ResourcesFacet;
 
 /**
  * @author <a href="mailto:wicem.zrelly@gmail.com">Wissem Zrelli</a>
  *
  */
 
-@FacetConstraints({
-         @FacetConstraint(DependencyFacet.class),
-         @FacetConstraint(MetadataFacet.class),
-         @FacetConstraint(ResourcesFacet.class)
-})
-public class DBMAFacet extends AbstractFacet<Project> implements ProjectFacet
+
+public interface DBMAFacet extends ProjectFacet
 {
-   public static final String DBMA_DIRECTORY_NAME = "migration";
-   public static final String DBMA_CONFIGURATION_FILE = "dbma.properties";
-   public static final String LIQUIBASE_DEFAULT_VERSION = "3.4.0";
-   public static final String LIQUIBASE_VERSION_PROPERTY_NAME = "LIQUIBASE_VERSION";
-
-   private DependencyBuilder createLiquibaseDependency()
-   {
-      return DependencyBuilder.create()
-               .setGroupId("org.liquibase")
-               .setArtifactId("liquibase-maven-plugin")
-               .setVersion(LIQUIBASE_DEFAULT_VERSION)
-               .setPackaging("pom")
-               .setScopeType("provided");
-   }
-
-   public String getLiquibaseDefaultVersion(){
-      return LIQUIBASE_DEFAULT_VERSION;
-   }
-   
-   public String getInstalledVersion()
-   {
-      MetadataFacet metadataFacet = getFaceted().getFacet(MetadataFacet.class);
-      return metadataFacet.getDirectProperty(LIQUIBASE_VERSION_PROPERTY_NAME);
-   }
-   
-   @Override
-   public boolean install()
-   {
-      DependencyFacet dependencyFacet = getFaceted().getFacet(DependencyFacet.class);
-      MetadataFacet metadataFacet = getFaceted().getFacet(MetadataFacet.class);
-
-      String version = metadataFacet.getDirectProperty(LIQUIBASE_VERSION_PROPERTY_NAME);
-      if (version == null)
-      {
-         createMigrationDirectory();
-         metadataFacet.setDirectProperty(LIQUIBASE_VERSION_PROPERTY_NAME, LIQUIBASE_DEFAULT_VERSION);
-         dependencyFacet.addDirectManagedDependency(createLiquibaseDependency());
-      }
-      return true;
-   }
-   
-   public void createMigrationDirectory(){
-      ResourcesFacet resourcesFacet = getFaceted().getFacet(ResourcesFacet.class);
-      resourcesFacet.getResourceDirectory().getOrCreateChildDirectory(DBMA_DIRECTORY_NAME);
-   } 
-
-   public boolean isInstalled()
-   {
-      DependencyFacet dependencyFacet = getFaceted().getFacet(DependencyFacet.class);
-      return dependencyFacet.hasDirectManagedDependency(createLiquibaseDependency());
-   }
-
    /**
-    * @param pBuilder
+    * set the properties file for Liquibase
+    * @param connection
     */
-   public void addDBMAProperties(DBMAPropertiesBuilder pBuilder)
-   {
-      // TODO Auto-generated method stub
-      
-   }
+   public void setPropertiesFile(ConnectionProfile connection);
+   
+   /**
+    * 
+    * @return properties used by Liquibase to perform migration 
+    */
+   public Properties getDBMAProperties();
 
 }
---
+++ src/main/java/org/jboss/forge/addon/dbma/facet/DBMAFacetImpl.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.dbma.facet;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.jboss.forge.addon.database.tools.connections.ConnectionProfile;
+import org.jboss.forge.addon.dbma.properties.DBMAPropertiesBuilder;
+import org.jboss.forge.addon.dbma.util.Constants;
+import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
+import org.jboss.forge.addon.facets.AbstractFacet;
+import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.facets.constraints.FacetConstraints;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.facets.DependencyFacet;
+import org.jboss.forge.addon.projects.facets.MetadataFacet;
+import org.jboss.forge.addon.projects.facets.ResourcesFacet;
+import org.jboss.forge.addon.resource.DirectoryResource;
+import org.jboss.forge.addon.resource.FileResource;
+
+/**
+ * @author <a href="mailto:wicem.zrelly@gmail.com">Wissem Zrelli</a>
+ *
+ */
+
+@FacetConstraints({
+   @FacetConstraint(DependencyFacet.class),
+   @FacetConstraint(MetadataFacet.class),
+   @FacetConstraint(ResourcesFacet.class)
+})
+
+public class DBMAFacetImpl extends AbstractFacet<Project> implements DBMAFacet
+{
+   
+   private DependencyBuilder createLiquibaseDependency()
+   {
+      return DependencyBuilder.create()
+               .setGroupId("org.liquibase")
+               .setArtifactId("liquibase-maven-plugin")
+               .setVersion(Constants.LIQUIBASE_DEFAULT_VERSION)
+               .setPackaging("pom")
+               .setScopeType("provided");
+   }
+
+   public String getLiquibaseDefaultVersion(){
+      return Constants.LIQUIBASE_DEFAULT_VERSION;
+   }
+   
+   public String getInstalledVersion()
+   {
+      MetadataFacet metadataFacet = getFaceted().getFacet(MetadataFacet.class);
+      return metadataFacet.getDirectProperty(Constants.LIQUIBASE_VERSION_PROPERTY_NAME);
+   }
+   
+   @Override
+   public boolean install()
+   {
+      DependencyFacet dependencyFacet = getFaceted().getFacet(DependencyFacet.class);
+      MetadataFacet metadataFacet = getFaceted().getFacet(MetadataFacet.class);
+
+      String version = metadataFacet.getDirectProperty(Constants.LIQUIBASE_VERSION_PROPERTY_NAME);
+      if (version == null)
+      {
+         createMigrationDirectory();
+         dependencyFacet.addDirectManagedDependency(createLiquibaseDependency());
+         metadataFacet.setDirectProperty(Constants.LIQUIBASE_VERSION_PROPERTY_NAME, Constants.LIQUIBASE_DEFAULT_VERSION);
+      }
+      return true;
+   }
+   
+   public void createMigrationDirectory(){
+      ResourcesFacet resourcesFacet = getFaceted().getFacet(ResourcesFacet.class);
+      resourcesFacet.getResourceDirectory().getOrCreateChildDirectory(Constants.DBMA_MIGRATION_DIRECTORY_NAME);
+   } 
+
+   public boolean isInstalled()
+   {
+      DependencyFacet dependencyFacet = getFaceted().getFacet(DependencyFacet.class);
+      return dependencyFacet.hasDirectManagedDependency(createLiquibaseDependency());
+   }
+   
+   public void setPropertiesFile(ConnectionProfile connection){
+      DBMAPropertiesBuilder pBuilder = DBMAPropertiesBuilder.create();
+      pBuilder.setConnection(connection).setLiquibaseVersion(Constants.LIQUIBASE_DEFAULT_VERSION);
+      
+      ResourcesFacet projectResourcesFacet = getFaceted().getFacet(ResourcesFacet.class);
+      DirectoryResource migrationDir = projectResourcesFacet.getResourceDirectory().getChildDirectory(Constants.DBMA_MIGRATION_DIRECTORY_NAME);
+      
+      FileResource<?> propertiesFile = migrationDir.getChild(Constants.DBMA_PROPERTIES_FILE).reify(FileResource.class);      
+      propertiesFile.setContents(pBuilder.toString()); 
+   }
+   
+   public Properties getDBMAProperties(){
+      Properties dbmaProperties = new Properties();
+      
+      ResourcesFacet resourcesFacet = getFaceted().getFacet(ResourcesFacet.class);
+      DirectoryResource migrationDir = resourcesFacet.getResourceDirectory().getChildDirectory(Constants.DBMA_MIGRATION_DIRECTORY_NAME);
+      try
+      {
+         dbmaProperties.load(migrationDir.getChild(Constants.DBMA_PROPERTIES_FILE).getResourceInputStream());
+      }
+      catch (IOException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return dbmaProperties;
+   }
+
+}
