bc0e219f1bed14b626fe0136db518bbdc0707777
addonis1990 <wicem.zrelly@gmail.com>
2015-07-02 12:14:44 +0100

    added constants

---
+++ src/main/java/org/jboss/forge/addon/dbma/util/Constants.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.dbma.util;
+
+/**
+ * @author <a href="mailto:wicem.zrelly@gmail.com">Wissem Zrelli</a>
+ *
+ */
+public class Constants
+{
+   public final static String MIGRATION_DIRECTORY_NAME = "migration";
+   public final static String PROPERTIES_FILE_NAME = "dbma.properties";
+   public static final String DBMA_DIRECTORY_NAME = "migration";
+   public static final String DBMA_CONFIGURATION_FILE = "dbma.properties";
+   public static final String LIQUIBASE_DEFAULT_VERSION = "3.4.0";
+   public static final String LIQUIBASE_VERSION_PROPERTY_NAME = "LIQUIBASE_VERSION";
+
+
+}
